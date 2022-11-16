b4cb7f9a1c77ad1111478da06af89838360690f4
Ananda S <asarangaram@hotmail.com>
2022-11-16 13:23:08 +0530

    remove pacakgeConfig,

--- example/dart_hocr.dart
+++ example/dart_hocr.dart
@@ -1,16 +1,7 @@
-import 'package:package_config/package_config.dart';
 import 'dart:io';
 import 'package:dart_hocr/dart_hocr.dart';
 
 void main(List<String> args) async {
-  var packageConfig = await findPackageConfig(Directory.current);
-  if (packageConfig != null) {
-    print(packageConfig.version);
-    /* for (var package in packageConfig.packages) {
-      print('- ${package.name}');
-    } */
-  }
-
   if (args.isNotEmpty && args.length == 2) {
     // ignore: unused_local_variable
     final doc =
